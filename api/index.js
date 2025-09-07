76947913fbfb97747db9b432419d3342807ecace
공보경 <rudqh9494@gmail.com>
2025-09-07 14:48:18 +0900

    chore:vercel js 연계 관련 추가 수정

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 // const port = 3000;
 
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
   console.error("hello world");
   res.send("Hello World!");
 });
