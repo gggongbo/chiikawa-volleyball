de4aefd7d198e93a0050b4f0815dfd30b8f6eec5
공보경 <rudqh9494@gmail.com>
2025-09-07 14:45:10 +0900

    fix:vercel function nodejs 연계를 위한 express app export

--- api/index.js
+++ api/index.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+// const port = 3000;
 
-app.get("/api/hello", (req, res) => {
+app.get("/", (req, res) => {
   console.error("hello world");
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.error(`서버가 http://localhost:${port} 에서 실행 중입니다`);
+// 추가 경로 처리
+app.get("/hello", (req, res) => {
+  console.error("hello world");
+  res.send("Hello World!");
 });
+
+module.exports = app;
+
+// app.listen(port, () => {
+//   console.error(`서버가 http://localhost:${port} 에서 실행 중입니다`);
+// });
